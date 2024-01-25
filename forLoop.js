689b00e3c41010e5574251fe326ec3f96d062977
Ishwari Kape <ishwarikape117@gmail.com>
2024-01-25 02:38:32 -0800

    forLoop.ts added

---
+++ forLoop.js
@@ -0,0 +1,4 @@
+var n = 1;
+for (var i = 10; i >= n; i--) {
+    console.log(i);
+}
---
+++ forLoop.ts
@@ -0,0 +1,5 @@
+//reverse the number from 1 to 10 using for loop
+var n:number=1;
+for(let i=10;i>=n;i--){
+    console.log(i);
+}
