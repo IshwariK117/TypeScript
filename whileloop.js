40584d901cc13acf36b074ab21e52a14d7bfee79
Ishwari Kape <ishwarikape117@gmail.com>
2024-01-25 01:26:17 -0800

    whileLoop.ts added

---
+++ whileloop.js
@@ -0,0 +1,7 @@
+//print 1 to 10 numbers using while loop
+var i = 1;
+while (i <= 10) {
+    console.log(i, " ");
+    i++;
+}
+//
---
+++ whileloop.ts
@@ -0,0 +1,8 @@
+//print 1 to 10 numbers using while loop
+var i:number=1;
+while(i<=10){
+    console.log(i," ");
+    i++;
+}
+
+//print numbers from 10 to 1
