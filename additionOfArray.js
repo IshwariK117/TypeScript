d821cd9ae086fbea038c40c9f9cbae7361c1d6a9
Ishwari Kape <ishwarikape117@gmail.com>
2024-01-25 09:30:40 -0800

    added

---
+++ additionOfArray.js
@@ -0,0 +1,8 @@
+//addition of elements in array
+var alphas;
+alphas = [1, 2, 3, 4];
+var sum = 0;
+for (var i = 0; i <= alphas[i]; i++) {
+    sum = sum + alphas[i];
+}
+console.log(sum);
---
+++ additionOfArray.ts
@@ -0,0 +1,9 @@
+
+//addition of elements in array
+var alphas:number[]
+alphas=[1,2,3,4]
+var sum:number=0;
+for(let i=0;i<=alphas[i];i++){
+    sum=sum+alphas[i];
+}
+console.log(sum)
